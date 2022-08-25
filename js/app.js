65c42bc413876b69c3743becb52a95dca4178034
nicoelias997 <nicoelias997@gmail.com>
2022-08-25 17:14:53 +0200

    Agrego vinculacion de una funcion cualquiera a las imagenes

---
+++ js/app.js
@@ -0,0 +1,29 @@
+const aquarius = document.getElementById("aquarius")
+const cancer = document.getElementById("cancer")
+const aries = document.getElementById("aries")
+const capricornio = document.getElementById("capricornio")
+const geminis = document.getElementById("geminis")
+const leo = document.getElementById("leo")
+const libra = document.getElementById("libra")
+const piscis = document.getElementById("piscis")
+const sagitario = document.getElementById("sagitario")
+const scorpio = document.getElementById("scorpio")
+const taurus = document.getElementById("taurus")
+const virgo = document.getElementById("virgo")
+
+function ventanaModal(id){
+    console.log("hola")
+}
+
+aquarius.addEventListener("click", ()=>{ventanaModal(aquarius)});
+cancer.addEventListener("click", ()=>{ventanaModal(cancer)});
+aries.addEventListener("click", ()=>{ventanaModal(aries)});
+capricornio.addEventListener("click", ()=>{ventanaModal(capricornio)});
+geminis.addEventListener("click", ()=>{ventanaModal(geminis)});
+leo.addEventListener("click", ()=>{ventanaModal(leo)});
+libra.addEventListener("click", ()=>{ventanaModal(libra)});
+piscis.addEventListener("click", ()=>{ventanaModal(piscis)});
+sagitario.addEventListener("click", ()=>{ventanaModal(sagitario)});
+scorpio.addEventListener("click", ()=>{ventanaModal(scorpio)});
+taurus.addEventListener("click", ()=>{ventanaModal(taurus)});
+virgo.addEventListener("click", ()=>{ventanaModal(virgo)});
